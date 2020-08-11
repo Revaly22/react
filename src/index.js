02625268c3df168d32ac4b2c2730373c8ed611e8
unknown <mosesrevaly@gmail.com>
2020-08-11 08:22:15 -0700

    app deployment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css"
+// adding props
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Person = ({img, name, job, children}) => {
+  // using template litrals
+  const url = `https://randomuser.me/api/portraits/thumb/women/${img}.jpg`;
+  return (
+    <article className = "person">
+      <img src={url} alt="me"></img>
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
+    </article>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// now create a component
+const PersonList = () => {
+  return (
+    <section className = "person-list">
+      <Person img="35" name="spider" job="web developer"></Person>
+      <Person img="22" name="revaly" job="doctor"></Person>
+      <Person img="57" name="esther" job="lawyer">
+        <p> this is esther she is a renowned lawyer in ghana with phd in law at university of ghana. </p>
+      </Person>
+    </section>
+  );
+};
+
+
+ReactDOM.render(<PersonList></PersonList>, document.getElementById("root"));
